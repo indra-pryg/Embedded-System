3e68d9975109574ad80d7e2e1302ce7286ba6c2a
indra-pryg <79032318+indra-pryg@users.noreply.github.com>
2021-02-14 11:42:32 +0700

    Monitoring Suhu Lingkungan
    Monitoring Suhu  Lingkungan Dengan Fuzzy Logic Metode Sugeno Menggunakan Arduino dan Sensor LM35 Berbasis Delphi

---
+++ Monitoring Suhu  Lingkungan Dengan Fuzzy Logic Metode Sugeno Menggunakan Arduino dan Sensor LM35 Menggunakan Software Delphi/Arduino/Monitoring Suhu/Monitoring Suhu.ino
@@ -0,0 +1,14 @@
+int val = 0;
+int tempPin = A0;
+char nilai;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+    val = analogRead(tempPin);
+    Serial.print(val);
+    Serial.println(" ");
+    delay(100);
+}
